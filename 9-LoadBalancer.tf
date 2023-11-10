dd4313f2dea7ec383e2671e9a56655d58bde263a
NitCloud88 <145623518+NitCloud88@users.noreply.github.com>
2023-11-10 11:29:15 -0500

    Update 9-LoadBalancer.tf
    Update Sao Paulo Project 5

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -1,30 +1,30 @@
-resource "aws_lb" "app1_alb" {
-  name               = "app1-load-balancer"
+resource "aws_lb" "app5_alb" {
+  name               = "app5-load-balancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.app1-sg02-LB01.id]
+  security_groups    = [aws_security_group.app5-sg02-LB01.id]
   subnets            = [
-    aws_subnet.public-eu-west-1a.id,
-    aws_subnet.public-eu-west-1b.id,
-    aws_subnet.public-eu-west-1c.id
+    aws_subnet.public-sa-east-1a.id,
+    aws_subnet.public-sa-east-1b.id,
+    aws_subnet.public-sa-east-1c.id
   ]
   enable_deletion_protection = false
 
   tags = {
     Name    = "App1LoadBalancer"
-    Service = "App1"
+    Service = "App5"
     Owner   = "User"
     Project = "Web Service"
   }
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app1_alb.arn
+  load_balancer_arn = aws_lb.app5_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app1_tg.arn
+    target_group_arn = aws_lb_target_group.app5_tg.arn
   }
 }
