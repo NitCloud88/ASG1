5242f7aaac060f4a18901949b983424dc4cc705c
NitCloud88 <145623518+NitCloud88@users.noreply.github.com>
2023-11-10 10:55:58 -0500

    Update 6-SG01-All.tf
    Update Sao Paulo Project 5

--- 6-SG01-All.tf
+++ 6-SG01-All.tf
@@ -1,7 +1,7 @@
-resource "aws_security_group" "app1-sg01-servers" {
-  name        = "app1-sg01-servers"
-  description = "app1-sg01-servers"
-  vpc_id      = aws_vpc.app1.id
+resource "aws_security_group" "app5-sg01-servers" {
+  name        = "app5-sg01-servers"
+  description = "app5-sg01-servers"
+  vpc_id      = aws_vpc.app5.id
 
   ingress {
     description = "MyHomePage"
@@ -36,10 +36,10 @@ resource "aws_security_group" "app1-sg01-servers" {
   }
 
   tags = {
-    Name    = "app1-sg01-servers"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "app5-sg01-servers"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 
 }
@@ -48,10 +48,10 @@ resource "aws_security_group" "app1-sg01-servers" {
 
 
 
-resource "aws_security_group" "app1-sg02-LB01" {
-  name        = "app1-sg02-LB01"
-  description = "app1-sg02-LB01"
-  vpc_id      = aws_vpc.app1.id
+resource "aws_security_group" "app5-sg02-LB01" {
+  name        = "app5-sg02-LB01"
+  description = "app5-sg02-LB01"
+  vpc_id      = aws_vpc.app5.id
 
   ingress {
     description = "MyHomePage"
@@ -69,10 +69,10 @@ resource "aws_security_group" "app1-sg02-LB01" {
   }
 
   tags = {
-    Name    = "app1-sg02-LB01"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "app5-sg02-LB01"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 
 }
