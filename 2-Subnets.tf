e3b955ea206d1c4cf26d9dc1b8962e8c2d080a54
NitCloud88 <145623518+NitCloud88@users.noreply.github.com>
2023-11-10 10:26:34 -0500

    Update 2-Subnets.tf
    Update Sao Paulo Project 5

--- 2-Subnets.tf
+++ 2-Subnets.tf
@@ -1,85 +1,85 @@
 #These are   for  public
 
-resource "aws_subnet" "public-eu-west-1a" {
-  vpc_id                  = aws_vpc.app1.id
-  cidr_block              = "10.32.1.0/24"
-  availability_zone       = "eu-west-1a"
+resource "aws_subnet" "public-sa-east-1" {
+  vpc_id                  = aws_vpc.app5.id
+  cidr_block              = "10.73.1.0/24"
+  availability_zone       = "sa-east-1"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "public-eu-west-1a"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "public-sa-east-1"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
 
-resource "aws_subnet" "public-eu-west-1b" {
-  vpc_id                  = aws_vpc.app1.id
-  cidr_block              = "10.32.2.0/24"
-  availability_zone       = "eu-west-1b"
+resource "aws_subnet" "public-sa-east-1b" {
+  vpc_id                  = aws_vpc.app5.id
+  cidr_block              = "10.73.2.0/24"
+  availability_zone       = "sa-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "public-eu-west-1b"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "public-sa-east-1b"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
 
 
-resource "aws_subnet" "public-eu-west-1c" {
-  vpc_id                  = aws_vpc.app1.id
-  cidr_block              = "10.32.3.0/24"
-  availability_zone       = "eu-west-1c"
+resource "aws_subnet" "public-sa-east-1c" {
+  vpc_id                  = aws_vpc.app5.id
+  cidr_block              = "10.73.3.0/24"
+  availability_zone       = "sa-east-1c"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "public-eu-west-1c"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "public-sa-east-1c"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
 
 #these are for private
-resource "aws_subnet" "private-eu-west-1a" {
-  vpc_id            = aws_vpc.app1.id
-  cidr_block        = "10.32.11.0/24"
-  availability_zone = "eu-west-1a"
+resource "aws_subnet" "private-sa-east-1a" {
+  vpc_id            = aws_vpc.app5.id
+  cidr_block        = "10.73.11.0/24"
+  availability_zone = "sa-east-1a"
 
   tags = {
-    Name    = "private-eu-west-1a"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "private-sa-east-1a"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
 
-resource "aws_subnet" "private-eu-west-1b" {
-  vpc_id            = aws_vpc.app1.id
-  cidr_block        = "10.32.12.0/24"
-  availability_zone = "eu-west-1b"
+resource "aws_subnet" "private-sa-east-1b" {
+  vpc_id            = aws_vpc.app5.id
+  cidr_block        = "10.73.12.0/24"
+  availability_zone = "sa-east-1b"
 
   tags = {
-    Name    = "private-eu-west-1b"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "private-sa-east-1b"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
 
 
-resource "aws_subnet" "private-eu-west-1c" {
-  vpc_id            = aws_vpc.app1.id
-  cidr_block        = "10.32.13.0/24"
-  availability_zone = "eu-west-1c"
+resource "aws_subnet" "private-sa-east-1c" {
+  vpc_id            = aws_vpc.app5.id
+  cidr_block        = "10.73.13.0/24"
+  availability_zone = "sa-east-1c"
 
   tags = {
-    Name    = "private-eu-west-1c"
-    Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Name    = "private-sa-east-1c"
+    Service = "application5"
+    Owner   = "Kings"
+    Planet  = "Success"
   }
 }
