3118a966702023505cfb9294149ce1b7b450e315
NitCloud88 <145623518+NitCloud88@users.noreply.github.com>
2023-11-10 10:03:37 -0500

    Update 1-VPC.tf
    Update Sao Paulo project 5

--- 1-VPC.tf
+++ 1-VPC.tf
@@ -1,11 +1,11 @@
 # this  makes  vpc.id which is aws_vpc.app1.id
-resource "aws_vpc" "app1" {
-  cidr_block = "10.32.0.0/16"
+resource "aws_vpc" "app5" {
+  cidr_block = "10.73.0.0/16"
 
   tags = {
-    Name = "app1"
-    Service = "application1"
-    Owner = "Chewbacca"
-    Planet = "Mustafar"
+    Name = "app5"
+    Service = "application5"
+    Owner = "Kings"
+    Planet = "Success"
   }
 }
